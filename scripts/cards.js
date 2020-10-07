d4dc7b4656a1c0f3589d502e24206b5831173b51
evilpaper <pellburt@gmail.com>
2020-10-07 07:02:49 +0200

    Remove finish game message when clicking outside the modal, move cards to own file

---
+++ scripts/cards.js
@@ -0,0 +1,50 @@
+const cards = [
+  {
+    name: "mrwrong",
+    img: "images/mrwrong.jpg",
+  },
+  {
+    name: "mrtickle",
+    img: "images/mrtickle.jpg",
+  },
+  {
+    name: "mrstrong",
+    img: "images/mrstrong.jpg",
+  },
+  {
+    name: "mrimpossible",
+    img: "images/mrimpossible.jpg",
+  },
+  {
+    name: "mrhappy",
+    img: "images/mrhappy.jpg",
+  },
+  {
+    name: "mrchatterbox",
+    img: "images/mrchatterbox.jpg",
+  },
+  {
+    name: "mrbump",
+    img: "images/mrbump.jpg",
+  },
+  {
+    name: "mrbounce",
+    img: "images/mrbounce.jpg",
+  },
+  {
+    name: "lmwhoops",
+    img: "images/lmwhoops.jpg",
+  },
+  {
+    name: "lmnaughty",
+    img: "images/lmnaughty.jpg",
+  },
+  {
+    name: "lmhug",
+    img: "images/lmhug.jpg",
+  },
+  {
+    name: "lmfun",
+    img: "images/lmfun.jpg",
+  },
+];
--- scripts/script.js
+++ scripts/script.js
@@ -16,56 +16,6 @@ const whoosh = document.querySelector(".whoosh");
 const vanish = document.querySelector(".vanish");
 const backgroundSong = document.querySelector(".background-song");
 const body = document.querySelector("body");
-const cards = [
-  {
-    name: "mrwrong",
-    img: "images/mrwrong.jpg",
-  },
-  {
-    name: "mrtickle",
-    img: "images/mrtickle.jpg",
-  },
-  {
-    name: "mrstrong",
-    img: "images/mrstrong.jpg",
-  },
-  {
-    name: "mrimpossible",
-    img: "images/mrimpossible.jpg",
-  },
-  {
-    name: "mrhappy",
-    img: "images/mrhappy.jpg",
-  },
-  {
-    name: "mrchatterbox",
-    img: "images/mrchatterbox.jpg",
-  },
-  {
-    name: "mrbump",
-    img: "images/mrbump.jpg",
-  },
-  {
-    name: "mrbounce",
-    img: "images/mrbounce.jpg",
-  },
-  {
-    name: "lmwhoops",
-    img: "images/lmwhoops.jpg",
-  },
-  {
-    name: "lmnaughty",
-    img: "images/lmnaughty.jpg",
-  },
-  {
-    name: "lmhug",
-    img: "images/lmhug.jpg",
-  },
-  {
-    name: "lmfun",
-    img: "images/lmfun.jpg",
-  },
-];
 
 let firstGuess = "";
 let secondGuess = "";
@@ -171,6 +121,13 @@ const createFinishGameView = () => {
     event.preventDefault();
     finishGameViewCloseButton.classList.add("apply-push");
     body.removeChild(finishGameView);
+  });
+  finishGameView.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    if (event.target.classList.contains("finished-game-view")) {
+      body.removeChild(finishGameView);
+    }
   });
 };
 
