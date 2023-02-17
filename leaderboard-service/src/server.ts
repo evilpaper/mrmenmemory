276f877fbed68342b7952a74d209eeaa618bfdaa
Pelle Lundgren <pellburt@gmail.com>
2023-02-17 21:00:06 +0100

    Feature/setup leaderboard service (#10)
    * Setup basic server
    
    * Add backend readme file
    
    * Setup basic server

---
+++ leaderboard-service/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("MR MEN MEMORY LEADERBOARD SERVICE");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
